9056db176382890eb3a1b7e794d7814c3f44eb3f
SMony-L <l.sovanmony@gmail.com>
2018-12-19 22:25:46 -0500

    Resize Problem fixed

--- GUI I/JS/asg9.js
+++ GUI I/JS/asg9.js
@@ -255,7 +255,7 @@ function load_droppable_targets() {
         // var pos = $("#the_board").position();
 
         var img_left = 10 * i;
-        var img_top = -125;
+        var img_top = -100;
 
         $("#board").append(drop);
 
